2e88655758581185ee72fbda045c5540abb19b2d
Merve Batman <mervebatman58@gmail.com>
2023-01-30 07:50:39 +0300

    "vuejs instance"

---
+++ 1-vue-instance/app.js
@@ -0,0 +1,31 @@
+const app = Vue.createApp({
+data(){
+    return {
+        title: "Vue.js Practices First Day",
+        content: "Lorem ipsum dolor sit amet.",
+        linkInfo: {
+            title: "Portfolyo için tıklayınız.",
+            target: "_blank",
+            url: "https://mervebatman.github.io",
+            alt: "portfolyo",
+        },
+        owner: "mervebatman",
+        coords: {
+            x: 0,
+            y: 0,
+        },
+    };
+},
+methods: {
+    changeTitle(title) {
+        this.title = title;
+    },
+    updateCoords(message, event) {
+        // console.log(message, event.x, event.y);
+        this.coords = {
+            x: event.x,
+            y: event.y,
+        }
+    }
+}
+}).mount("#app"); 
